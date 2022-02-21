16610c2040f7573f06113ac058f257d3d2086e05
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-20 19:39:44 -0700

    Started kingsland blockchain exercise

---
+++ main.js
@@ -0,0 +1,47 @@
+'use strict'
+var CryptoJS = require('crypto-js');
+var express = require('express');
+var bodyParser = require('body-parser');
+var WebSocket = require('ws');
+
+class Block {
+    constructor(index, previousHash, timestamp, data, hash) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = hash.toString();
+    }
+}
+
+var getGenesisBlock = () => {
+	return new Block(
+		0,
+		"0",
+		1465154705,
+		"my genesis block!!",
+		"816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"
+	);
+};
+
+var blockchain = [getGenesisBlock()];
+
+function testApp() {
+    function showBlockchain(inputBlockchain) {
+        for (let i = 0; i < inputBlockchain.length; i++) {
+            console.log(inputBlockchain[i]);
+        }
+
+        console.log();
+    }
+
+    showBlockchain(blockchain);
+}
+
+var getLatestBlock = () => blockchain[blockchain.length - 1];
+
+var calculateHash = (index, previousHash, timestamp, data) => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+};
+
+testApp();
