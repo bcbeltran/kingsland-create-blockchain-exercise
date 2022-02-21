1db287e9c50531e8456d30c2c556192f9c802f24
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-21 11:19:20 -0700

    Created the HTTP server

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var WebSocket = require('ws');
 
+var http_port = process.env.HTTP_PORT || 3001;
+
+var sockets = [];
+
+var initHttpServer = () => {
+    var app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
+    app.post('/mineBlock', (req, res) => {
+        var newBlock = generateNextBlock(req.body.data);
+        addBlock(newBlock);
+        console.log('Block added: ' + JSON.stringify(newBlock));
+        res.send();
+    });
+    app.get('/peers', (req, res) => {
+        res.send(sockets.map(s => s._socket.remoteAddress + ':' + s._socket.remotePort));
+    });
+    app.post('/addPeer', (req, res) => {
+        res.send();
+    });
+    app.listen(http_port, () => console.log("Listening http on port: " + http_port));
+}
+
 class Block {
     constructor(index, previousHash, timestamp, data, hash) {
         this.index = index;
