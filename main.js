f29128cbf8517c4faa10fca0063559e0b6fc11b1
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-20 19:55:12 -0700

    Created functions to verify blockchain validity and add it to the blockchain

--- main.js
+++ main.js
@@ -27,21 +27,55 @@ var getGenesisBlock = () => {
 var blockchain = [getGenesisBlock()];
 
 function testApp() {
-    function showBlockchain(inputBlockchain) {
-        for (let i = 0; i < inputBlockchain.length; i++) {
-            console.log(inputBlockchain[i]);
-        }
+    // function showBlockchain(inputBlockchain) {
+    //     for (let i = 0; i < inputBlockchain.length; i++) {
+    //         console.log(inputBlockchain[i]);
+    //     }
 
-        console.log();
-    }
+    //     console.log();
+    // }
 
-    showBlockchain(blockchain);
+    // showBlockchain(blockchain);
+    console.log(calculateHashForBlock(getGenesisBlock()));
 }
 
 var getLatestBlock = () => blockchain[blockchain.length - 1];
 
 var calculateHash = (index, previousHash, timestamp, data) => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+};
+
+var calculateHashForBlock = (block) => {
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+};
+
+var generateNextBlock = (blockData) => {
+    var previousBlock = getLatestBlock();
+    var nextIndex = previousBlock.index + 1;
+    var nextTimestamp = new Date().getTime() / 1000;
+    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
+};
+
+var isValidNewBlock = (newBlock, previousBlock) => {
+    if (previousBlock.index + 1 !== newBlock.index) {
+        console.log('Invalid index');
+        return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+        console.log("Invalid previous hash");
+        return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+        console.log(typeof (newBlock.hash) + ' ' + typeof calculateHashForBlock(newBlock));
+        console.log("Invalid hash: " + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+        return false;
+    }
+    return true; 
+};
+
+var addBlock = (newBlock) => {
+    if (isValidNewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+    }
 };
 
 testApp();
